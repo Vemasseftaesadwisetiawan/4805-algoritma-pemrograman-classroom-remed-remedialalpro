f8a2cc15ca32eba4945e7e0d8719ebf3956ff463
Vemas seftaesa dwi setiawan <vmasstwn@gmail.com>
2025-01-22 14:07:13 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,19 @@
 public class Nomor1{
      public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);  
+    
+        System.out.print("Total km: ");  
+        int totalKilometer = scanner.nextInt();  
+   
+        double honorTaksi;  
+        if (totalKilometer < 3) {  
+            honorTaksi = 20000;  
+        } else {  
+            honorTaksi = 20000 + (totalKilometer - 3) * 10000;  
+        }  
         
+        System.out.println("Honor taksi: " + honorTaksi);  
+
+        scanner.close(); 
     }
 }
